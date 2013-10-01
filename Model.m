db3b3bc83fa645fcdbbb19e2099029b883c5ed5b
ROBERT GERALD DICK <rgd5032@waring107-lab-mac141.clc-labs.its.psu.edu>
2013-10-01 15:38:04 -0400

    Initial UI setup; handled content inset and offset for keyboard

---
+++ Model.m
@@ -0,0 +1,13 @@
+//
+//  Model.m
+//  Penn State Search
+//
+//  Created by ROBERT GERALD DICK on 10/1/13.
+//  Copyright (c) 2013 ROBERT GERALD DICK. All rights reserved.
+//
+
+#import "Model.h"
+
+@implementation Model
+
+@end
--- Penn State Search/RGDViewController.m
+++ Penn State Search/RGDViewController.m
@@ -7,8 +7,14 @@
 //
 
 #import "RGDViewController.h"
+#import "Model.h"
 
-@interface RGDViewController ()
+#define kAccessIDTextFieldTag 2
+#define kKeyboardPortaitHeight 216
+
+@interface RGDViewController () <UITextFieldDelegate>
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
+@property (strong, nonatomic) Model *model;
 
 @end
 
@@ -18,12 +24,35 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    _model = [[Model alloc] init];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    [textField resignFirstResponder];
+    return YES;
+}
+
+-(void)textFieldDidBeginEditing:(UITextField *)textField
+{
+    if (textField.tag == kAccessIDTextFieldTag)
+    {
+        UIEdgeInsets edgeInsets = UIEdgeInsetsMake(0, 0, kKeyboardPortaitHeight, 0);
+        [self.scrollView setContentInset:edgeInsets];
+        [self.scrollView setContentOffset:CGPointMake(0, kKeyboardPortaitHeight)];
+    }
+}
+
+-(void)textFieldDidEndEditing:(UITextField *)textField
+{
+    [self.scrollView setContentInset:UIEdgeInsetsZero];
+    [self.scrollView setContentOffset:CGPointZero];
 }
 
 @end
